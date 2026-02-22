828c969979d2e03dba5464c0d9969d0dad682d25
Abhinav M <130998983+abhinav0700@users.noreply.github.com>
2026-02-22 11:03:15 +0000

    hello

--- src/connection.py
+++ src/connection.py
@@ -37,6 +37,7 @@ class Connection:
     def close(self):
         """Close the connection."""
         print("hlo")
+        print("this is thechange")
         self._is_open = False
 
     def is_open(self):
